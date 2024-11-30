7fb79998720c8e96b93a998810d93ea30e46831a
hbizahin <hbizahin@gmail.com>
2024-12-01 01:37:48 +0600

    made fake datas, header, blogs

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Header from './components/header/Header'
+import Blogs from './components/Blogs/Blogs'
+function App() {
+
+  return (
+    <>
+      <Header></Header>
+      <Blogs></Blogs>
+    </>
+  )
+}
+
+export default App
